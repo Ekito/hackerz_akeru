84f4a55827eb762adfe0177112e6e0f701295ab3
Arnaud Boudou <skuld@me.com>
2014-03-22 10:40:05 +0100

    Get coordinates from NEMA frame

--- arduino.ino
+++ arduino.ino
@@ -8,6 +8,8 @@ struct coord {
   float longitude;
 };
 
+char nema[] = "$GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77";
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
@@ -21,8 +23,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Begin");
   
-  coord coordinates = {43.256663, 1.199009};
+  //coord coordinates = {43.256663, 1.199009};
   
+  coord coordinates = extractCoordinates(nema);
   
   digitalWrite(13, HIGH);
   akeru.send(&coordinates, sizeof(coord));
@@ -33,4 +36,76 @@ void loop() {
   delay(30*1000);
   
   Serial.println("After delay");
+}
+
+
+// Extract coordinates from NEMA data
+struct coord extractCoordinates(char data[]) {
+  char *frame_type;
+  char *timestamp;
+  char *state;
+  char *latitude;
+  char *latitude_direction;
+  char *longitude;
+  char *logitude_direction;
+  
+  char *brkb;
+  
+  frame_type = strtok_r(data, "," ,&brkb);
+  timestamp = strtok_r(NULL, "," ,&brkb);
+  state = strtok_r(NULL, "," ,&brkb);
+  latitude = strtok_r(NULL, "," ,&brkb);
+  latitude_direction = strtok_r(NULL, "," ,&brkb);
+  longitude = strtok_r(NULL, "," ,&brkb);
+  logitude_direction = strtok_r(NULL, "," ,&brkb);
+
+  if (strcmp(frame_type, "$GPRMC") == 0) {
+    // Got the correct frame type
+    if (strcmp(state, "A") == 0) {
+      // GPS locked
+      
+      // Get latitude and logitude (DDMM.MMMM style ; DD = degrees; MM = minutes)
+      float lat = atof(latitude);
+      float lon = atof(longitude);
+      if (strcmp(latitude_direction, "S") == 0) {
+        // If southern coordinates => negative values
+        lat = 0 - lat;
+      }
+      if (strcmp(logitude_direction, "W") == 0) {
+        // If western coordinates => positive values
+        lon = 0 - lon;
+      }
+      
+      // Convert latitude and longitude to decimal degrees
+      float dlat = calculateCurrentLocation(lat);
+      float dlon = calculateCurrentLocation(lon);
+
+      coord result = {dlat, dlon};
+      return result;
+    }
+  }
+
+  coord result = {NULL, NULL};
+  return result;
+}
+
+// Convert coordinates from GPS to decimal degrees
+float calculateCurrentLocation(float coordinate){
+
+  float minutesLat; 
+  float minuteLong;
+  float degreesLat; 
+  float degreesLong; 
+  float secondsLat; 
+  float millisecondsLat; 
+
+  degreesLat = trunc(coordinate/100);
+  minutesLat = coordinate - (degreesLat*100);
+  secondsLat = (minutesLat - trunc(minutesLat)) * 60;
+  millisecondsLat = (secondsLat - trunc(secondsLat)) * 1000;
+  
+  minutesLat = trunc(minutesLat);
+  secondsLat = trunc(secondsLat);
+
+  return degreesLat + minutesLat/60 + secondsLat/3600 + millisecondsLat/3600000; 
 }
